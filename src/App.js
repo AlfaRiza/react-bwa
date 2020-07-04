658f18c11abf16d781b20d88102d6db55e770527
AlfaRiza <malfariza45@gmail.com>
2020-07-05 03:11:27 +0700

    web design part 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-    const userLogger = "Alfahmada";
-    const userLoggerJsx = <strong> Alfa For Me </strong>;
-
-    return (<div className="App" >
-        {userLogger}
-        {userLoggerJsx}
-    </div>
+    const category = "RUNNING";
+    const price = 175000;
+    return (
+        <div class="Parentbox">
+            <div class="Foto">
+                <img src="foto.jpeg" alt="" />
+            </div>
+            <div class="Deskripsi">
+                <h1 class="Cate">{category}</h1>
+                <p class="Title">Jaket Koding</p>
+                <p class="Price">IDR. {price}</p>
+                <p class="Info">
+                    jaket design edisi koding, terdiri dari framework dan tools yang sering digunakan ngoding sehingga terlihat keren
+                </p>
+            </div>
+        </div>
     );
 }
 
